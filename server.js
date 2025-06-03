3957397e7476d16adcbf9da23fec93e4fb462678
ashishkuma8847 <ashishkuma8847@gmail.com>
2025-06-03 12:47:14 +0530

    fdsf

--- server.js
+++ server.js
@@ -1,40 +1,59 @@
 import sequelize from "./src/config/db.js";
-import express from "express"
-import dotenv from "dotenv"
-import cors from "cors"
-import  "./src/models/user.js";
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import "./src/models/user.js";
 import productrouter from "./src/router/productitems.router.js";
 import homeitemsrouter from "./src/router/homeitems.router.js";
 import userrouter from "./src/router/user.router.js";
 import routerhd from "./src/router/homedatar.router.js";
 import hotdealmenu from "./src/router/hotdealmenu.router.js";
 import hotdealside from "./src/router/Hotdealsidemenu.router.js";
+import path from "path";
+import { fileURLToPath } from "url";
 
-dotenv.config()
+dotenv.config();
 
-const app = express()
+const app = express();
 
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 
-const PORT = process.env.PORT
+const PORT = process.env.PORT;
 
-app.listen(PORT,()=>{
-    console.log("port  is :" ,PORT)
-})
+// For ES module support (__dirname workaround)
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+console.log(__dirname, "__dirname");
 
+// Serve static files in /uploads/images
+app.use(
+  "/upload",
+  express.static(path.join(__dirname, "uploads/images/"))
+);
 
 async function connection() {
-    try {
-        await sequelize.authenticate()
-        console.log("authenticate")
-        await sequelize.sync({alter:true})
-        console.log("sync")
-    } catch (error) {
-        console.error(error);
-        
-    }
+  try {
+    await sequelize.authenticate();
+    console.log("authenticate");
+    await sequelize.sync({ alter: true });
+    console.log("sync");
+  } catch (error) {
+    console.error(error);
+  }
 }
-connection()
+connection();
 
-app.use("/api",userrouter,routerhd,homeitemsrouter,productrouter,hotdealmenu,hotdealside)
+app.use(
+  "/api",
+  userrouter,
+  routerhd,
+  homeitemsrouter,
+  productrouter,
+  hotdealmenu,
+  hotdealside
+);
+
+app.listen(PORT, () => {
+  console.log("port  is :", PORT);
+});
