1448c735d4dcbb2ff99c01fc61ad672a014d495e
ashishkuma8847 <ashishkuma8847@gmail.com>
2025-06-22 12:09:19 +0530

    first commit

--- src/controller/cart.controller.js
+++ src/controller/cart.controller.js
@@ -41,29 +41,29 @@ if(!cartItems) {
 };
 
 // // Update quantity
-// export const updateItem = async (req, res) => {
-//   const { userId, productId, quantity } = req.body;
-//   try {
-//     const item = await Cart.findOne({ where: { userId, productId } });
-//     if (item) {
-//       item.quantity = quantity;
-//       await item.save();
-//       res.json(item);
-//     } else {
-//       res.status(404).json({ message: "Item not found" });
-//     }
-//   } catch (err) {
-//     res.status(500).json({ message: err.message });
-//   }
-// };
+export const updateItem = async (req, res) => {
+  const { userId, productId, quantity } = req.body;
+  try {
+    const item = await Cart.findOne({ where: { userId, productId } });
+    if (item) {
+      item.quantity = quantity;
+      await item.save();
+      res.json(item);
+    } else {
+      res.status(404).json({ message: "Item not found" });
+    }
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
 
-// // Remove item
-// export const removeItem = async (req, res) => {
-//   const { userId, productId } = req.body;
-//   try {
-//     await Cart.destroy({ where: { userId, productId } });
-//     res.json({ message: "Removed" });
-//   } catch (err) {
-//     res.status(500).json({ message: err.message });
-//   }
-// };
+// Remove item
+export const removeItem = async (req, res) => {
+  const { userId, productId } = req.body;
+  try {
+    await Cart.destroy({ where: { userId, productId } });
+    res.json({ message: "Removed" });
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
--- src/router/cart.router.js
+++ src/router/cart.router.js
@@ -1,7 +1,9 @@
 import express from "express"
-import { addToCart, getCart } from "../controller/cart.controller.js"
+import { addToCart, getCart, removeItem, updateItem } from "../controller/cart.controller.js"
 
 const cartRouter= express.Router()
 cartRouter.post("/addToCart",addToCart)
 cartRouter.get("/getcart/:userId",getCart )
+cartRouter.put("/cartupdate", updateItem);
+cartRouter.delete("/cartremove", removeItem);
 export default cartRouter
